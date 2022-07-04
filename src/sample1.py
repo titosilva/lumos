f4c8d6a8aed22aebf52d231b911ed58296821aff
titosilva <jt.mat@hotmail.com>
2022-07-04 00:09:20 -0300

    feat: improved sample1.py

--- src/sample1.py
+++ src/sample1.py
@@ -1,7 +1,18 @@
-from core.default_signals import d
+from core.default_signals import d, u
 from core.signal import n
 from core.signal_plot import SignalPlotter
 
 if __name__ == "__main__":
-    print(d[0])
-    SignalPlotter.plot(d[n-3], (-5, 5))
+    # You can easily define a signal by using the 'n' imported from core.signal
+    x1 = n * 2 + 2
+    # You may apply the signal to an input and see the results
+    print(f"x[2] = {x1[2]}")
+    # You may also plot a signal and see its shape
+    SignalPlotter.plot(x1, (-5, 5))
+
+    # A nice way to define complex signals is by using 'constructing blocks'
+    # Some basic signals are defined on core.default_signals and may be used 
+    # 'u' is an alias to unit_step and 'd' is an alias to unit_impulse
+    x2 = u[n] - u[n-4] - 0.5 * d[n-1]
+    # You may also add the '[n]' after a signal, if you want to
+    SignalPlotter.plot(x2[n], (-5, 5))

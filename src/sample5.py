bc5ebd46af6ece4e6b580e6182f176ba4ce3edf6
titosilva <jt.mat@hotmail.com>
2022-07-06 22:36:22 -0300

    feat: improved sample5.py

--- src/sample5.py
+++ src/sample5.py
@@ -12,32 +12,39 @@ if __name__ == "__main__":
     # but also increases the plotting time
     divisions = 200 
 
+    # X(z) = z**(-1) + z**(-2) + z**(-3) + z**(-4)
     x1 = d[n - 1] + d[n - 2] + d[n - 3] + d[n - 4]
-    # X1(z) = z + z ** 2 + z ** 3 + z ** 4
     Hx1 = Z(x1)
-    # PlotUtils.plot_complex_function(Hx1, limits, limits, divisions)
 
-    x1_v2 = signal_from_values([0, 1, 1, 1, 1])
+    # X(z) = 1 + z**(-1) + z**(-2) + z**(-3)
+    x1_v2 = x1[n + 1]
     Hx1_v2 = Z(x1_v2)
-    # PlotUtils.plot_complex_function(Hx1_v2, limits, limits, divisions)
+
+    # X(z) = z + 1 + z**(-1) + z**(-2)
+    x1_v3 = x1[n + 2]
+    Hx1_v3 = Z(x1_v3)
+
+    PlotUtils.plot_many_complex_functions([
+        (Hx1, '$x[n]$', limits, limits, None),
+        (Hx1_v2, '$x[n + 1]$', limits, limits, None),
+        (Hx1_v3, '$x[n + 2]$', limits, limits, None)
+    ], rows=1, divisions=divisions)
 
     # Let's test it against the unit step
-    # PlotUtils.plot_complex_function(Z[u], limits, limits, divisions, lambda z: np.abs(z) > 1)
+    PlotUtils.plot_complex_function(Z[u], limits, limits, divisions, lambda z: np.abs(z) > 1)
 
     x2 = dcos[pi * n / 3] * u[n]
     # Z[...] is also valid
     Hx2 = Z[x2]
-    # PlotUtils.plot_complex_function(Hx2, limits, limits, divisions, lambda z: np.abs(z) > 1)
+    PlotUtils.plot_complex_function(Hx2, limits, limits, divisions, lambda z: np.abs(z) > 1)
 
     a = 0.6 + 0.8j
     x3 = dexp(a) * u[n]
     Hx3 = Z[x3]
-    # PlotUtils.plot_complex_function(Hx3, limits, limits, divisions, lambda z: np.abs(z) > np.abs(a))
 
     a = 0.6 + 0.8j
     x4 = -1 * dexp(a) * u[-1 * n - 1]
     Hx4 = Z[x4]
-    # PlotUtils.plot_complex_function(Hx4, limits, limits, divisions, lambda z: np.abs(z) < np.abs(a))
 
     PlotUtils.plot_many_complex_functions([
         (Hx3, '$a^nu[n]$', limits, limits, lambda z: np.abs(z) > np.abs(a)),
